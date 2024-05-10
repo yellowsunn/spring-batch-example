d90add1b67e0b4000b4a97102563b1c3cf09d8ab
yellowsunn <chohankook95@gmail.com>
2024-05-10 13:08:21 +0900

    init project

---
+++ src/main/kotlin/com/yellowsunn/springbatchexample/SpringBatchExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.yellowsunn.springbatchexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBatchExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBatchExampleApplication>(*args)
+}
