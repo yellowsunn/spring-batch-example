451266eb2620f083219d0617ce511f2366a3aae2
yellowsunn <chohankook95@gmail.com>
2024-05-10 15:49:22 +0900

    파라미터 추가

--- src/main/kotlin/com/yellowsunn/springbatchexample/config/SimpleJobConfig.kt
+++ src/main/kotlin/com/yellowsunn/springbatchexample/config/SimpleJobConfig.kt
@@ -3,11 +3,13 @@ package com.yellowsunn.springbatchexample.config
 import org.slf4j.LoggerFactory
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.JobScope
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
 import org.springframework.batch.core.step.tasklet.TaskletStep
 import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.PlatformTransactionManager
@@ -20,18 +22,24 @@ class SimpleJobConfig(
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     @Bean
-    fun simpleJob(): Job {
+    fun simpleJob(
+        simpleStep1: Step,
+        simpleStep2: Step,
+    ): Job {
         return JobBuilder("simpleJob", jobRepository)
-            .start(simpleStep1())
-            .next(simpleStep2())
+            .start(simpleStep1)
+            .next(simpleStep2)
             .build()
     }
 
     @Bean
-    fun simpleStep1(): Step {
+    @JobScope
+    fun simpleStep1(
+        @Value("#{jobParameters['requestDate']}") requestDate: String,
+    ): Step {
         return StepBuilder("simpleStep1", jobRepository)
             .tasklet({ contribution, chunkContext ->
-                logger.info("step1 executed.")
+                logger.info("step1 executed.requestDate={}", requestDate)
                 RepeatStatus.FINISHED
             }, transactionManager)
             .build()
